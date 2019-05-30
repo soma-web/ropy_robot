bd39fb70cb9997c7864af150bab846056f35ca3a
soma <andi.scholz@gmail.com>
2019-05-30 13:45:17 +0200

    working android version

---
+++ Android Project/ropy/app/src/main/java/de/soma/ropy/RopyActivity.java
@@ -0,0 +1,352 @@
+package de.soma.ropy;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+public class RopyActivity extends Activity {
+
+    TextView tvFeedback;
+    BluetoothAdapter mBTAdapter;
+    Handler mHandler;
+    Handler valueSendHandler;
+    ThrottleView throttle;
+    ImageView ivIndicator;
+    float throttleValue;
+    float sendThrottleValue;
+
+    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
+    private BluetoothSocket mBTSocket = null; // bi-directional client-to-client data path
+
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+    private static final String RopyBTID = "3C:71:BF:AA:AB:5A";
+    private static final String RopyBTName = "RopyBT";
+
+    // #defines for identifying shared types between calling functions
+    private final static int REQUEST_ENABLE_BT = 1; // used to identify adding bluetooth names
+    private final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
+    private final static int CONNECTING_STATUS = 3; // used in bluetooth handler to identify message status
+
+    private static final String TAG = "RopyActivity";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.ropy_activity);
+
+        Button btSend = findViewById(R.id.btConnect);
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //discover();
+                connectByUUIDAndName(RopyBTID, RopyBTName);
+            }
+        });
+
+        Button btStop = findViewById(R.id.btStop);
+        btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //discover();
+               if(mConnectedThread != null){
+                   mConnectedThread.write("{\"motor\":{\"speed\":0}}");
+                   tvFeedback.setText(getResources().getString(R.string.stop));
+               }
+            }
+        });
+
+        throttle = (ThrottleView)findViewById(R.id.throttleView);
+        throttle.SetChangeListener(new ThrottleView.ThrottleListener() {
+            @Override
+            public void OnValueChanged(float value) {
+                throttleValue = value;
+            }
+        });
+
+        tvFeedback = findViewById(R.id.tvFeedback);
+        tvFeedback.setText(getString(R.string.status_unconnected));
+        ivIndicator = findViewById(R.id.ivIndicator);
+
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter(); // get a handle on the bluetooth radio
+        mHandler = new Handler(){
+            public void handleMessage(android.os.Message msg){
+                Log.d(TAG, "handler message " + msg.toString());
+                if(msg.what == MESSAGE_READ){
+                    String readMessage = null;
+                    try {
+                        readMessage = new String((byte[]) msg.obj, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        Log.e(TAG, "error in handler " + e);
+                        e.printStackTrace();
+                    }
+                    tvFeedback.setText("message " + readMessage);
+                }
+
+                if(msg.what == CONNECTING_STATUS){
+                    if(msg.arg1 == 1)
+                    {
+
+                        tvFeedback.setText(String.format(getResources().getString(R.string.status_connected), (String)(msg.obj)));
+                        ivIndicator.setImageDrawable(getResources().getDrawable(R.drawable.green_dot));
+                        throttle.Activate();
+                    }
+                    else
+                    {
+                        ivIndicator.setImageDrawable(getResources().getDrawable(R.drawable.red_dot));
+                        tvFeedback.setText(getResources().getString(R.string.status_connect_fail));
+                    }
+                }
+            }
+        };
+
+        valueSendHandler = new Handler();
+
+        Runnable runner = new Runnable() {
+            @Override
+            public void run() {
+                if(sendThrottleValue != throttleValue && mConnectedThread != null) {
+                    mConnectedThread.write("{\"motor\":{\"speed\":" + throttleValue + "}}");
+                    tvFeedback.setText(String.format(getResources().getString(R.string.sending_value), String.format("%.2f", throttleValue)));
+                    sendThrottleValue = throttleValue;
+                }
+                valueSendHandler.postDelayed(this, 200);
+            }
+        };
+        valueSendHandler.postDelayed(runner, 300);
+
+        //TODO check if bluetooth is available and on
+        //TODO connect to device
+        //TODO handle disconnection
+        //TODO bring feedback
+        //TODO send values in an interval
+
+
+    }
+
+    private void discover(){
+        // Check if the device is already discovering
+        if(mBTAdapter.isDiscovering()){
+            mBTAdapter.cancelDiscovery();
+            Toast.makeText(getApplicationContext(),"Discovery stopped",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            if(mBTAdapter.isEnabled()) {
+                mBTAdapter.startDiscovery();
+                Toast.makeText(getApplicationContext(), "Discovery started", Toast.LENGTH_SHORT).show();
+                registerReceiver(blReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+            }
+            else{
+                tvFeedback.setText(getResources().getString(R.string.no_bluetooth));
+                Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    final BroadcastReceiver blReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // add the name to the list
+                if(device.getName().equals(RopyBTName)){
+                    Log.d(TAG, "Ropy found");
+                    connectToRopy(device);
+                }
+
+            }
+        }
+    };
+
+    private void connectByUUIDAndName(final String address, final String name){
+        if(!mBTAdapter.isEnabled()) {
+            Toast.makeText(getBaseContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        tvFeedback.setText(getResources().getString(R.string.status_connecting));
+        ivIndicator.setImageDrawable(getResources().getDrawable(R.drawable.blue_dot));
+        // Spawn a new thread to avoid blocking the GUI one
+        new Thread()
+        {
+            public void run() {
+                boolean fail = false;
+
+                BluetoothDevice device = mBTAdapter.getRemoteDevice(address);
+                Log.d(TAG, "Bluetooth device " + device.getName() + " " + device.getAddress());
+                try {
+                    mBTSocket = createBluetoothSocket(device);
+                } catch (IOException e) {
+                    fail = true;
+                    Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
+                }
+                // Establish the Bluetooth socket connection.
+                try {
+                    mBTSocket.connect();
+                } catch (IOException e) {
+                    try {
+                        fail = true;
+                        mBTSocket.close();
+                        mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
+                                .sendToTarget();
+                    } catch (IOException e2) {
+                        //insert code to deal with this
+                        Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                if(fail == false) {
+                    mConnectedThread = new RopyActivity.ConnectedThread(mBTSocket);
+                    mConnectedThread.start();
+
+                    mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, name)
+                            .sendToTarget();
+                }
+            }
+        }.start();
+    }
+
+
+    private void connectToRopy(final BluetoothDevice device){
+        if(!mBTAdapter.isEnabled()) {
+            Toast.makeText(getBaseContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        tvFeedback.setText("Connecting to " + RopyBTName + "....");
+
+        new Thread()
+        {
+            public void run() {
+                boolean fail = false;
+
+                Log.d(TAG, "Bluetooth device " + device.getName() + " " + device.getAddress());
+
+                try {
+                    mBTSocket = createBluetoothSocket(device);
+                } catch (IOException e) {
+                    fail = true;
+                    Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
+                }
+                // Establish the Bluetooth socket connection.
+                try {
+                    mBTSocket.connect();
+                } catch (IOException e) {
+                    try {
+                        fail = true;
+                        mBTSocket.close();
+                        mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
+                                .sendToTarget();
+                    } catch (IOException e2) {
+                        //insert code to deal with this
+                        Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                if(fail == false) {
+                    mConnectedThread = new RopyActivity.ConnectedThread(mBTSocket);
+                    mConnectedThread.start();
+
+                    mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, RopyBTName)
+                            .sendToTarget();
+                }
+            }
+        }.start();
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        Log.d(TAG, "creating socket for " + device.toString());
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            Log.d(TAG, "method: " + m);
+            return (BluetoothSocket) m.invoke(device, BTMODULEUUID);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+        }
+        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.available();
+                    if(bytes != 0) {
+                        buffer = new byte[1024];
+                        SystemClock.sleep(100); //pause and wait for rest of data. Adjust this depending on your sending speed.
+                        bytes = mmInStream.available(); // how many bytes are ready to be read?
+                        bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
+                        mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                                .sendToTarget(); // Send the obtained bytes to the UI activity
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String input) {
+            input += '\n';
+            Log.d(TAG, "btcommand: " + input);
+            byte[] bytes = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+    }
+
+}
